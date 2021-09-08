0ac0f26b7c3693f894013a0319aabf342ab0294a
webobscure <50839940+webobscure@users.noreply.github.com>
2021-09-08 17:29:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+ readTextFile("users.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
+
+
