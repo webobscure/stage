36e1b8cea7e65a56b3bcdec80cf7091dc52b8f05
webobscure <50839940+webobscure@users.noreply.github.com>
2021-09-12 10:19:27 +0300

    4/7

--- script.js
+++ script.js
@@ -511,15 +511,120 @@ let myArray = [
           <td class='firstName'>${data[i].name.firstName}</td>
           <td class='secondName'>${data[i].name.lastName}</td>
           <td class='about'>${data[i].about} </td>
-          <td class='eyeColor'>${data[i].eyeColor}</td>
+          <td class='eye-color' style="background-color: ${data[i].eyeColor}; color: ${data[i].eyeColor};">${data[i].eyeColor}</td>
           </tr>`
           table.innerHTML += row
         }
+
   }
 
-  let sortedRows = Array.from(table.rows)
-  .slice(1)
-  .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
+  th = document.getElementsByTagName("th")
 
-table.tBodies[0].append(...sortedRows);
+  for( c = 0; c <th.length; c++) {
+    th[c].addEventListener("click", item(c))
+  }
+
+
+  function item(c) {
+
+    return function() {
+      console.log(c);
+      sortTable(c)
+    }
+  }
+
+  function sortTable(c) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 0; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[c];
+        y = rows[i + 1].getElementsByTagName("TD")[c];
+        //check if the two rows should switch place:
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+
+  //Форма редактирования
+function editTableData() {
+  const table = document.querySelector('table'),
+        editForm = document.querySelector('.form-wrapper'),
+        inputs = editForm.querySelectorAll('input'),
+        textarea = editForm.querySelector('textarea'),
+        btnEdit = editForm.querySelector('.btn-edit'),
+        btnClose = editForm.querySelector('.btn-close');
+
+  let CHANGE_ROW; // строка tr которую нужно будет редактировать
+
+  //Используется делегирование событий. При клике на таблицу получает строку по которой кликнули и отображает рядом с ней форму редактирования
+  table.addEventListener('click', function(event) {
+    const row = event.target.closest('.data-row'); //возвращает ближайщего предка соответствующего селектору.
+    
+    CHANGE_ROW = row;
+
+    if (!row) return; //проверка, содержит ли в себе event.target строку row
+    if (!table.contains(row)) return; //проверка, прендалежит ли row нашей таблице.
+
+    editForm.style.cssText = `display: block;  top: ${row.offsetTop}px; left: ${row.offsetWidth + 20}px;`;
+
+    inputs[0].value = row.cells[0].innerHTML;
+    inputs[1].value = row.cells[1].innerHTML;
+    textarea.value = row.cells[2].innerHTML.slice(0, row.cells[2].innerHTML.length - 3);
+    inputs[2].value = row.cells[3].firstChild.innerHTML;
+  });
   
+  //При нажатии на кнопку редактирования btnEdit содержимое ячеек строки заменяется на содержимое формы
+  btnEdit.addEventListener('click', () => {    
+    //узнаем длину массива, что бы узнать arraySize из функции splitArray. На случай если сделаю чтобы юзер задавал значение arraySize
+    const editedRow = {
+      id: CHANGE_ROW.id ,
+      name: {
+        firstName: inputs[0].value,
+        lastName: inputs[1].value,
+      },
+      phone: null,
+      about: textarea.value,
+      eyeColor: inputs[2].value,
+    }
+
+    let editedRowIndex = 0;
+
+    jsonData.JSON.forEach((item, i, arr) => {
+      if (item.id === CHANGE_ROW.id) {
+        arr.splice(i, 1, editedRow);
+        editedRowIndex = i + 1; 
+      }
+    })
+
+    localStorage.setItem('jsonData', JSON.stringify(jsonData));
+    editForm.style='';
+
+    renderCell(jsonData, Math.ceil(editedRowIndex / (jsonData.JSON.length / rowAmount))); // (jsonData.length / rowAmount) - arraySize из функции splitArray
+  });
+
+  btnClose.addEventListener('click', () => editForm.style=''); // закрывает форму редактирования.
+}
+
